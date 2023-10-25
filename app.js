0f8034b3aeb40bcd547f752fb7ad5e8c9fdf5e44
Kamil Pigulak <kamil.pigulak@mediaflex.pl>
2023-10-25 15:26:28 +0200

    change CORS

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type", "Authorization");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With,Content-Type, Authorization, Accept");
   next();
 });
 
@@ -28,6 +28,10 @@ app.use((error, req, res, next) => {
     message: message,
     data: data,
   });
+});
+
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Not Found" });
 });
 
 mongoose
